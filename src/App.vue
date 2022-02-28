69dd7d05b87b5415a1b800a005ee630e6b30072a
Alessandro Terracciano <terracciano41@gmail.com>
2022-02-28 09:49:48 +0100

    Struttura iniziale

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+
+    <MyHeader/>
+    <MyMain/>
+   
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyMain from './components/MyMain.vue'
+
+const axios = require('axios');
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyMain 
+  },
+
+  data(){
+    return {
+
+
+    }
+  },
+  methods: {
+      getFilms() {
+
+    axios.get('/user?ID=12345')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,31 @@
+<template>
+
+<header>
+    <form>
+
+        <input type="text" v-model="searchText">
+        <button type="submit">Ricerca</button>
+
+    </form>
+</header>
+  
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+
+    data() {
+        return {
+            searchText : ''
+        }
+    }
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
